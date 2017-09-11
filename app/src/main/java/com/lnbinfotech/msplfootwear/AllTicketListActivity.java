786777aa9626399ef64d734e5e0e4f6fec34a47f
paitsystems <paitsystems@gmail.com>
2017-09-11 13:12:05 +0530

    Sep 09,17

--- app/src/main/java/com/lnbinfotech/msplfootwear/AllTicketListActivity.java
+++ app/src/main/java/com/lnbinfotech/msplfootwear/AllTicketListActivity.java
@@ -41,7 +41,7 @@ public class AllTicketListActivity extends AppCompatActivity {
     static String selStat;
     AllTicketListAdapter adapter;
     EditText ed_search;
-    //Comment Added
+    //Test Branch Entry
     AdView mAdView;
 
     @Override
